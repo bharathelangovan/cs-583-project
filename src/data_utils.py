bab175989b364996e25c932f2b97f4ea5fc6e211
Eric Faurie <efaurie@hawk.iit.edu>
2015-03-02 20:06:21 -0600

    Initial Commit

---
+++ src/data_utils.py
@@ -0,0 +1,9 @@
+from graph import DirectedGraph, Node, Edge
+
+def load_linqs_data(content_file, cites_file):
+    '''
+    Create a DirectedGraph object and add Nodes and Edges
+    This is specific to the data files provided at http://linqs.cs.umd.edu/projects/projects/lbc/index.html
+    Return two items 1. graph object, 2. the list of domain labels (e.g., ['AI', 'IR'])
+    '''
+    raise NotImplementedError('You need to implement this method')
